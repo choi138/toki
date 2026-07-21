1e2c9390b2d4c5a8cdff5705b982a84b7de5050d
choi138 <kidjustinchoi@gmail.com>
2026-07-22 08:32:37 +0900

    build(remote): add durable sync foundations

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "TokiRemote",
+    platforms: [
+        .macOS(.v13),
+    ],
+    products: [
+        .library(name: "TokiSyncProtocol", targets: ["TokiSyncProtocol"]),
+        .library(name: "TokiDurableStorage", targets: ["TokiDurableStorage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.5"),
+    ],
+    targets: [
+        .target(name: "TokiDurableStorage"),
+        .target(
+            name: "TokiSyncProtocol",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto"),
+            ]),
+        .testTarget(
+            name: "TokiSyncProtocolTests",
+            dependencies: ["TokiSyncProtocol"]),
+    ])
