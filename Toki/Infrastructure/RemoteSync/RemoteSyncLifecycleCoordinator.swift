03c1462384fe4d62bb2ac16953eb1475af72109c
choi138 <kidjustinchoi@gmail.com>
2026-07-24 13:23:04 +0900

    fix(remote): remove lifecycle lock callbacks

--- Toki/Infrastructure/RemoteSync/RemoteSyncLifecycleCoordinator.swift
+++ Toki/Infrastructure/RemoteSync/RemoteSyncLifecycleCoordinator.swift
@@ -17,23 +17,17 @@ final class RemoteSyncLifecycleCoordinator: @unchecked Sendable {
     }
 
     func validate(_ ticket: ReadTicket) throws {
-        try commit(ticket) {}
-    }
-
-    func commit<Value>(_ ticket: ReadTicket, operation: () throws -> Value) throws -> Value {
         lock.lock()
         defer { lock.unlock() }
         guard generation == ticket.generation else {
             throw RemoteSyncLifecycleError.stateChanged
         }
-        return try operation()
     }
 
-    func mutate<Value>(_ operation: () throws -> Value) throws -> Value {
+    func invalidateReadTickets() {
         lock.lock()
         defer { lock.unlock() }
         generation &+= 1
-        return try operation()
     }
 }
 
